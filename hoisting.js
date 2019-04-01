6e42212996226fbece482a00aa35b53770868334
Patrick B Brown <pbbrown1112@gmail.com>
2019-04-01 17:56:53 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -20,12 +20,12 @@ function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
 
-  
+
 
   function sayMy() {
     var name = "Kristin";
     console.log(name);
       }
-      
+
   sayMy();
 }
